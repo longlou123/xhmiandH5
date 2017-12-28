f76a591fd6e1bef41efa8eb084a1d7dd7e3ca848
龙楼 <longlou@longloudeMacBook-Pro.local>
2017-12-28 19:27:52 +0800

    1

---
+++ src/pages/test.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h1>我是测试页面啦啦啦！！</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ .hello{
+    height:600px; 
+    background: red;
+ }
+</style>
