f815fdf09c8d7c1774c8e57a6e2ac019451fd213
龙楼 <longlou@longloudeMacBook-Pro.local>
2017-12-29 11:07:56 +0800

    1

--- src/pages/test1.vue
+++ src/pages/test1.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="header">
-    <div @click='test'>{{hand}}</div>
+    <div  class="div">    
+      <button @click='test' type="">{{hand}}</button>
+    </div>
+    <div class="one">
+       <p @click="go">11111</p>
+    </div>
+      <div class="two">
+         <h1>22222</h1>
+      </div>
   </div>
 </template>
 
@@ -10,38 +18,48 @@
     name:'test',
     data(){
       return {
-            hand:"我是测试的哟"
+          hand:"我是测试的哟"
       }
     },
     methods:{
       test(){
         Toast('我是Tip');
+      },
+      go(){
+        this.$router.push({ path: '/test2'});
       }
     }
   }
 
 </script>
-<style scoped>
+<style lang="scss" scoped>
   .header {
     /*position: fixed;*/
     z-index: 99;
     width: 100%;
     background: #f55947;
     height: 3rem;
-    font: {
-      size: 1.2rem;
-      weight: 100;
-    }
-  ;
-    div {
+    font-size: 16px;
+  }
+    .div {
+      height:50px; 
       text-align: center;
-      line-height: 3rem;
-      color: #fff;
-      font: {
-        weight: 300;
+      line-height: 50px;
+      color: #000;
+      font-size: 16px;
+      button{
+        background: #fff;
       }
-    ;
     }
+  .one{
+    width:1rem;
+    height:1rem;
+    background:#fff;
+  }
+  .two{
+    width:5rem;
+    height:5rem;
+    background: #ADFF2F;
   }
 
 </style>
