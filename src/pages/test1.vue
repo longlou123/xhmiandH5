1b0ef9337ab7d7d5e4b62b4870ccc5cef3e02d93
龙楼 <longlou@longloudeMacBook-Pro.local>
2017-12-29 13:03:51 +0800

    1

--- src/pages/test1.vue
+++ src/pages/test1.vue
@@ -13,7 +13,7 @@
 </template>
 
 <script type="text/ecmascript-6">
-  import {DatetimePicker, Button, Toast} from 'mint-ui';
+  import {DatetimePicker, Button, Toast} from 'mint-ui';//引入miti ui功能
   export default {
     name:'test',
     data(){
@@ -23,7 +23,11 @@
     },
     methods:{
       test(){
-        Toast('我是Tip');
+        Toast({
+          message: '提示',
+          position: 'top',
+          duration: 3000
+        });
       },
       go(){
         this.$router.push({ path: '/test2'});
